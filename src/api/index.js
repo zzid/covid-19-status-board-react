2012d5c62647ce2d3c814f34ef32c212f05da683
zzid <931010dy@gmail.com>
2020-06-08 10:05:51 +0900

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const url ='https://covid19.mathdro.id/api'
+
+
+// async
+export const fetchData = async ()=>{
+    try{
+        const { data : {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        return {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        };
+
+    }catch(error){
+
+    }
+}
+
+
+export const fetchDailyData = async() =>{
+    try{
+        const response = await axios.get('${url}/daily');
+
+    }catch(error){
+
+    }
+}
