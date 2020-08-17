c20966b4e3f69f7b82dde38ffda69945f66ca272
zzid <931010dy@gmail.com>
2020-08-17 22:26:33 +0900

    test commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -32,7 +32,12 @@ const Cards = ({ data : {confirmed, recovered, deaths, lastUpdate } })=>{
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered </Typography>
                         <Typography variant="h5" >
-                        <CountUp start={0} end ={recovered.value} duration ={2.5} separator=","/>
+                        <CountUp 
+                            start={0} 
+                            end ={recovered.value} 
+                            duration ={2.5} 
+                            separator=","
+                            />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of recoveries from COVID-19</Typography>
@@ -42,7 +47,12 @@ const Cards = ({ data : {confirmed, recovered, deaths, lastUpdate } })=>{
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths </Typography>
                         <Typography variant="h5" >
-                            <CountUp start={0} end ={deaths.value} duration ={2.5} separator=","/>
+                            <CountUp 
+                                start={0} 
+                                end ={deaths.value} 
+                                duration ={2.5} 
+                                separator=","
+                                />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant = "body2">Number of deaths of caused by COVID-19</Typography>
