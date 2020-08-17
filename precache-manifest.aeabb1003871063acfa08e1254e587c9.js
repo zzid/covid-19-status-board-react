49ec55a869fd4676a12646bd7fd6d1361d11e95e
zzid <931010dy@gmail.com>
2020-08-17 22:04:04 +0900

    rebase?

---
+++ precache-manifest.aeabb1003871063acfa08e1254e587c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47bb4ce4d26244bd2ffedee868daace",
+    "url": "/covid-19-status-board-react/index.html"
+  },
+  {
+    "revision": "f2dc462e5cd937518012",
+    "url": "/covid-19-status-board-react/static/css/main.1960ab1b.chunk.css"
+  },
+  {
+    "revision": "3ae9f5314801714fe19a",
+    "url": "/covid-19-status-board-react/static/js/2.32d46363.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-status-board-react/static/js/2.32d46363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dc462e5cd937518012",
+    "url": "/covid-19-status-board-react/static/js/main.cbb7b3ef.chunk.js"
+  },
+  {
+    "revision": "108424b719ac82a02009",
+    "url": "/covid-19-status-board-react/static/js/runtime-main.0373e4e5.js"
+  }
+]);
