51ae68e6ec549e3da443da85a0793835ef30c878
zzid <931010dy@gmail.com>
2020-08-17 22:21:01 +0900

    test commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -32,7 +32,7 @@ const Chart = ({data : {confirmed, deaths, recovered}, country})=>{
                     borderColor : '#3333ff',
                     fill : true,
                 }, {
-                    data : dailyData.map(({ deaths }) =>deaths),
+                    data : dailyData.map(({ deaths }) => deaths),
                     label : 'deaths',
                     borderColor : 'red',
                     backgroundColor : 'rgba(255, 0, 0, 0.5)',
